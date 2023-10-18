5ae24a6d927895a1837893c29c6a70d1723788b9
Denis Klimov <plitnichenko@gmail.com>
2023-10-18 20:21:42 +0300

    Hotfix: Check static rpc client connection

--- src/hooks/useStaticRpc.ts
+++ src/hooks/useStaticRpc.ts
@@ -1,8 +1,20 @@
 import { ProviderRpcClient } from 'everscale-inpage-provider'
-import { EverscaleStandaloneClient } from 'everscale-standalone-client'
+import { EverscaleStandaloneClient, checkConnection } from 'everscale-standalone-client'
+import { ConnectionProperties } from 'everscale-standalone-client/client/ConnectionController'
 
 import { debug } from '@/utils'
 
+
+const MAINNET_JRPC: ConnectionProperties = 'mainnetJrpc'
+const MAINNET_GQL: ConnectionProperties = {
+    data: {
+        endpoints: [
+            'https://mainnet.evercloud.dev/89a3b8f46a484f2ea3bdd364ddaee3a3/graphql',
+        ],
+    },
+    id: 42,
+    type: 'graphql',
+}
 
 let rpc: ProviderRpcClient
 
@@ -13,9 +25,12 @@ export function useStaticRpc(): ProviderRpcClient {
             'color: #bae701',
         )
         rpc = new ProviderRpcClient({
-            fallback: () => EverscaleStandaloneClient.create({
-                connection: 'mainnetJrpc',
-            }),
+            fallback: () => checkConnection('mainnetJrpc')
+                .then(() => MAINNET_JRPC)
+                .catch(() => MAINNET_GQL)
+                .then(props => EverscaleStandaloneClient.create(
+                    { connection: props },
+                )),
             forceUseFallback: true,
         })
     }
